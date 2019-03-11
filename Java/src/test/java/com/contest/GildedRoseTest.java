20fcfbbf2590da3e0d0adb1e3f1ef5e1c01e3493
martyleedan <568769863@qq.com>
2019-03-11 20:39:37 +0800

    理解错误从头再来

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
-/*    @Test
+    @Test
     public void should_fix_me() {
         Item[] items = new Item[] { new Item("normal", 10, 20) };
         GildedRose gildedRose = new GildedRose(items);
@@ -15,65 +15,5 @@ public class GildedRoseTest {
 
         assertEquals(9, (gildedRose.items[0]).sellIn);
         assertEquals(19, gildedRose.items[0].quality);
-    }*/
-	/*    @Test
-    public void should_quality_20_if_sellin_10() {
-        Item[] items = new Item[] { new Item("normal", 10, 20) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(9, (gildedRose.items[0]).sellIn);
-        assertEquals(19, gildedRose.items[0].quality);
-    }*/
-    @Test
-    public void should_quality_20_if_sellin_10() {
-        Item[] items = new Item[] { new Item("normal", 8, 18) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(8, gildedRose.items[0].sellIn);
-        assertEquals(18, gildedRose.items[0].quality);
-    }
-    @Test
-    public void should_quality_10_if_sellin_0() {
-        Item[] items = new Item[] { new Item("normal", 0, 10) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(0, gildedRose.items[0].sellIn);
-        assertEquals(10, gildedRose.items[0].quality);
-    }
-    @Test
-    public void should_quality_8_if_sellin_fu1() {
-        Item[] items = new Item[] { new Item("normal", -1, 8) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(-1, gildedRose.items[0].sellIn);
-        assertEquals(8, gildedRose.items[0].quality);
-    }
-    @Test
-    public void should_quality_8_if_sellin_fu5() {
-        Item[] items = new Item[] { new Item("normal", -5, 0) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(-5, gildedRose.items[0].sellIn);
-        assertEquals(0, gildedRose.items[0].quality);
-    }
-    @Test
-    public void should_quality_8_if_sellin_fu6() {
-        Item[] items = new Item[] { new Item("normal", -6, 0) };
-        GildedRose gildedRose = new GildedRose(items);
-        
-        gildedRose.updateQuality();
-
-        assertEquals(-6, gildedRose.items[0].sellIn);
-        assertEquals(0, gildedRose.items[0].quality);
     }
 }
