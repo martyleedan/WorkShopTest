7325e4c31477ea5d9c3c776309c4a8b1bf0937d3
martyleedan <568769863@qq.com>
2019-03-11 19:26:09 +0800

    单元测试例子方法命名优化

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -6,8 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
-    @Test
+/*    @Test
     public void should_fix_me() {
+        Item[] items = new Item[] { new Item("normal", 10, 20) };
+        GildedRose gildedRose = new GildedRose(items);
+        
+        gildedRose.updateQuality();
+
+        assertEquals(9, (gildedRose.items[0]).sellIn);
+        assertEquals(19, gildedRose.items[0].quality);
+    }*/
+    @Test
+    public void should_quality_20_if_sellin_10() {
         Item[] items = new Item[] { new Item("normal", 10, 20) };
         GildedRose gildedRose = new GildedRose(items);
         
