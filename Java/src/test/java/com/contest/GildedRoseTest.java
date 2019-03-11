f245d76cd5e36ef6519fbc8dd990b344b6915134
martyleedan <568769863@qq.com>
2019-03-11 19:31:51 +0800

    修改demo测试方法取值，demo方法取值不合理，超出给定初始化值

--- Java/src/test/java/com/contest/GildedRoseTest.java
+++ Java/src/test/java/com/contest/GildedRoseTest.java
@@ -17,8 +17,17 @@ public class GildedRoseTest {
         assertEquals(19, gildedRose.items[0].quality);
     }*/
     @Test
-    public void should_quality_20_if_sellin_10() {
+/*    public void should_quality_20_if_sellin_10() {
         Item[] items = new Item[] { new Item("normal", 10, 20) };
+        GildedRose gildedRose = new GildedRose(items);
+        
+        gildedRose.updateQuality();
+
+        assertEquals(9, (gildedRose.items[0]).sellIn);
+        assertEquals(19, gildedRose.items[0].quality);
+    }*/
+    public void should_quality_20_if_sellin_10() {
+        Item[] items = new Item[] { new Item("normal", 8, 18) };
         GildedRose gildedRose = new GildedRose(items);
         
         gildedRose.updateQuality();
